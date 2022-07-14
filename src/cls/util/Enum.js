553e4d791f2b2c9e15fbabf158ec1f40df8c1192
Qoqqi <sazykin.georgii@gmail.com>
2022-07-14 23:12:42 +0300

    Удалена зависимость enumify (теперь используется свой класс Enum)

--- src/cls/util/Enum.js
+++ src/cls/util/Enum.js
@@ -1,5 +1,38 @@
-import {Enumify} from 'enumify';
+export default class Enum {
 
-export default class Enum extends Enumify {
+    static closeEnum() {
+        const enumKeys = [];
+        const enumValues = [];
 
+        for (const [key, value] of Object.entries(this)) {
+
+            value.enumKey = key;
+            value.enumOrdinal = enumValues.length;
+
+            enumKeys.push(key);
+            enumValues.push(value);
+        }
+
+        this.enumKeys = enumKeys;
+        this.enumValues = enumValues;
+    }
+
+    static enumValueOf(str) {
+        const index = this.enumKeys.indexOf(str);
+
+        if (index >= 0) {
+            return this.enumValues[index];
+        }
+
+        return undefined;
+    }
+
+    static [Symbol.iterator]() {
+        return this.enumValues[Symbol.iterator]();
+    }
+
+    toString() {
+        // noinspection JSUnresolvedVariable
+        return this.constructor.name + '.' + this.enumKey;
+    }
 }
