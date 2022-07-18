7e75cc5a2ae169bd36ff8a5861a4bfcfc18336e7
Qoqqi <sazykin.georgii@gmail.com>
2022-07-18 04:09:13 +0300

    Отключен StrictMode (из-за двойных срабатываний коллбеков)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    // Если включить StrictMode, функции жизненного цикла компонентов вызываются дважды
+    // <React.StrictMode>
         <App />
-    </React.StrictMode>
+    // </React.StrictMode>
 );
