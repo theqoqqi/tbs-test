d93690cb50b3bac583cd3f9593c0be58621b60e9
Qoqqi <sazykin.georgii@gmail.com>
2022-07-20 06:13:09 +0300

    Фикс

--- src/components/ui/ActionPointsBar/index.js
+++ src/components/ui/ActionPointsBar/index.js
@@ -25,6 +25,7 @@ export default class ActionPointsBar extends React.Component {
 
             let $point = (
                 <div
+                    key={i}
                     className={classNames('action-point', {
                         'usable': usable,
                         'used': used,
