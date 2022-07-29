7a424286bb45c17664c40378b6d6f52e2329bdc1
Qoqqi <sazykin.georgii@gmail.com>
2022-07-29 13:04:00 +0300

    Рефакторинг

--- src/data/scripts/abilities/index.js
+++ src/data/scripts/abilities/index.js
@@ -2,9 +2,9 @@
 export {default as apShard} from './apShard';
 export {default as chest} from './chest';
 export {default as burningAttack} from './burningAttack';
-export {default as defaultMelee} from './system/defaultMelee';
-export {default as defaultRanged} from './system/defaultRanged';
-export {default as defaultRun} from './system/defaultRun';
 
+export {default as defaultMelee} from './common/defaultMelee';
+export {default as defaultRanged} from './common/defaultRanged';
+export {default as defaultRun} from './common/defaultRun';
 export {default as greediness} from './common/greediness';
 export {default as longAttack} from './common/longAttack';
