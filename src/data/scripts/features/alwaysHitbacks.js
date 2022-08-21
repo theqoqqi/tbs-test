8a8c230ccd1d611cc42e87d4bc40565dc63a1946
Qoqqi <sazykin.georgii@gmail.com>
2022-08-21 05:58:55 +0300

    Фикс способности

--- src/data/scripts/features/alwaysHitbacks.js
+++ src/data/scripts/features/alwaysHitbacks.js
@@ -1,7 +1,18 @@
 import AbilitySlot from '../../../cls/enums/AbilitySlot.js';
 import PawnDamageReceivedEvent from '../../../cls/events/types/PawnDamageReceivedEvent.js';
+import PawnProps from '../../../cls/pawns/props/PawnProps.js';
+import HitbackFrequency from '../../../cls/enums/HitbackFrequency.js';
 
 export default {
+    /** @this Feature */
+    modifyPawnProperty({propertyName, value}) {
+
+        if (propertyName === PawnProps.hitbackFrequency) {
+            return HitbackFrequency.ALWAYS;
+        }
+
+        return value;
+    },
     /** @this Feature */
     getEvents() {
         return [
@@ -10,8 +21,10 @@ export default {
                 let owner = this.owner;
                 let victim = event.victim;
                 let attacker = event.attacker;
+                let hitIndex = event.hitInfo.indexInHitChain;
+                let isHitbackApplicable = fight.moveExecutor.isHitbackApplicable(attacker, victim, event.ability);
 
-                if (victim === owner && fight.moveExecutor.shouldHitback(attacker, victim, event.ability)) {
+                if (victim === owner && hitIndex === 1 && isHitbackApplicable) {
                     let ability = fight.getAbilityInSlot(owner, AbilitySlot.REGULAR);
 
                     fight.moveExecutor.attack(victim, attacker, ability, 2);
