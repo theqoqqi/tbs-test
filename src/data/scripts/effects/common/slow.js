353042ab8cfe07716ae9f125e3916d060cd26951
Qoqqi <sazykin.georgii@gmail.com>
2022-08-21 06:08:28 +0300

    Фикс эффекта замедления

--- src/data/scripts/effects/common/slow.js
+++ src/data/scripts/effects/common/slow.js
@@ -1,8 +1,21 @@
 import PawnProps from '../../../../cls/pawns/props/PawnProps.js';
+import PawnEffectAddedEvent from '../../../../cls/events/types/PawnEffectAddedEvent.js';
 
 export default {
     /** @this Effect */
     propertyBonuses: {
         [PawnProps.actionPoints]: -1,
+    },
+    /** @this Effect */
+    getEvents() {
+        return [
+            [PawnEffectAddedEvent, event => {
+                if (event.effect !== this) {
+                    return;
+                }
+
+                this.owner.consumeActionPoints(1);
+            }],
+        ];
     }
 }
